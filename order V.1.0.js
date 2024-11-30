1c6be3d26eb391931795b884a3ec5d80abf4ae89
Sourav Mahapatra <souravmahaptra213@gmail.com>
2024-11-30 22:44:52 +0530

    First Commit

---
+++ order V.1.0.js
@@ -0,0 +1,40 @@
+
+// shortcut order //
+const Orders = [
+    {
+        productName: 'Mobile',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Laptop',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Headphone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Camera',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'Gaming Console',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'VR',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    }
+]
---
+++ order full V.1.0.js
@@ -0,0 +1,82 @@
+
+// full order //
+const Orders = [
+    {
+        productName: 'Mobile',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Laptop',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Headphone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Camera',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'Gaming Console',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'VR',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'Football',
+        productNumber: '85631',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Cricket Bat',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+    {
+        productName: 'Drone',
+        productNumber: '49347',
+        paymentStatus: 'Due',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'Camera',
+        productNumber: '96996',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'TV',
+        productNumber: '22821',
+        paymentStatus: 'Paid',
+        shipping: 'Pending',
+    },
+    {
+        productName: 'AC',
+        productNumber: '00482',
+        paymentStatus: 'Paid',
+        shipping: 'Delivered',
+    },
+    {
+        productName: 'Fridge',
+        productNumber: '36378',
+        paymentStatus: 'Refunded',
+        shipping: 'Declined',
+    },
+]
